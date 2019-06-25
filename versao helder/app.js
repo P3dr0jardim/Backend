058dfe71a70427f59fc3adaa146299416a30467a
Hélder Bacanhim <43825339+phn0m@users.noreply.github.com>
2019-06-25 09:47:57 +0100

    ds

--- versao helder/app.js
+++ versao helder/app.js
@@ -8,16 +8,24 @@ const fs = require('fs');
 var today = new Date();
 
 function writelog(data) {
-    console.log(data)
     var date = today.getDate() + '-' + (today.getMonth() + 1) + '-' + today.getFullYear();
     var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
     var dateTime = date + ' ' + time;
     data += " " + dateTime + "\n";
-    fs.appendFile('logs/logs-' + date + '.txt', data, (err) => {
+    fs.appendFile('logs/logs-sessions.txt', data, (err) => {
         if (err) throw err;
     });
 }
 
+function writechat(data) {
+    var date = today.getDate() + '-' + (today.getMonth() + 1) + '-' + today.getFullYear();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var dateTime = date + ' ' + time;
+    data += " " + dateTime + "\n";
+    fs.appendFile('logs/logschat-' + date + '.txt', data, (err) => {
+        if (err) throw err;
+    });
+}
 
 
 app.set('view engine', 'ejs'); // set up ejs for templating
@@ -68,85 +76,74 @@ var io = require('socket.io')(server);
 app.get('/', function (req, res) {
     res.render('index.ejs');
 });
-app.post('/upload', (req, res) => {
-    upload(req, res, (err) => {
-        if (err) {
-            res.render('index.ejs', {
-                msg: err
-            });
-        } else {
-            if (req.file == undefined) {
-                res.render({
-                    msg: 'Error: No File Selected!'
-                });
-            } else {
-         //       io.sockets.emit('broadcast_message', {
-         //           message: "<img src='./public/uploads/+" + req.file.filename + "'>",
-         //           username: socket.username
-          //      });
-                res.render('index.ejs', {
-                    msg: 'File Uploaded!',
-                    file: `uploads/${req.file.filename}`
-                });
-            }
-        }
-    });
-});
-
-function writechat(data) {
-    console.log(data)
-    var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    var dateTime = date + ' ' + time;
-    data += " " + dateTime + "\n";
-    fs.appendFile('logs/logschat-' + date + '.txt', data, (err) => {
-        if (err) throw err;
-    });
-}
-
-
-
 
 // Registar o evento Connection
 
 var userlist = [];
 io.on("connection", (socket) => {
+    app.post('/', (req, res) => {
+        upload(req, res, (err) => {
+            if (err) {
+                res.render('index.ejs', {
+                    msg: err
+                });
+            } else {
+                if (req.file == undefined) {
+                    res.render({
+                        msg: 'Error: No File Selected!'
+                    });
+                } else {
+                    io.sockets.emit('broadcast_message', {
+                        message: `uploads/${req.file.filename}`,
+                        username: socket.username
+                    });
+                    // res.render('index.ejs', {
+                    //     msg: 'File Uploaded!',
+                    //     file: `uploads/${req.file.filename}`
+                    // });
+                }
+            }
+        });
+    });
+
     socket.username = "USER " + uuid();
     writelog("User " + socket.username + " Connected");
     socket.on("send_message", (data) => {
-        if(data.message[0]=="@"){
-            var separar=data.message.split(":")
-            var nome="";
-            for(var i=1;i<separar[0].length;i++){
-                nome+=separar[0][i]
+        if (data.message[0] == "@") {
+            var separar = data.message.split(":")
+            var nome = "";
+            for (var i = 1; i < separar[0].length; i++) {
+                nome += separar[0][i]
             }
-            destid=userlist.nome;
-            if(destid!=null){
-                writechat("User " + socket.username + "send to "+nome+":"+separado[1])
+            destid = userlist.nome;
+            if (destid != null) {
+                writechat("User " + socket.username + "send to " + nome + ": " + separado[1])
                 io.to(destid).emit('private_message', {
-                message: separado[1],
-                username: socket.username,
-                destusername: nome
-            })
-            io.to(socket.id).emit('message_true',{
-                message:separado[1],
-                username:nome
-            })
-        }else{
-            var myname=socket.name
-            io.to(myname).emit('erro_user',{
-                message:"User not exist"
-            })
+                    message: separado[1],
+                    username: socket.username,
+                    destusername: nome
+                })
+                io.to(socket.id).emit('message_true', {
+                    message: separado[1],
+                    username: nome
+                })
+            } else {
+                var myname = socket.name
+                io.to(myname).emit('erro_user', {
+                    message: "User not exist"
+                })
+            }
+        } else {
+            if (data.message != "") {
+                writechat(socket.username + ": " + data.message)
+                io.sockets.emit('broadcast_message', {
+                    message: data.message,
+                    username: socket.username
+                })
+            }
         }
-    }else{
-        writechat(socket.username + ":" + data.message)
-        io.sockets.emit('broadcast_message', {
-            message: data.message,
-            username: socket.username
-        })
-    }
-});
-    socket.on("user_connected", (data) => {
+    });
+    socket.on("user_connected", () => {
         io.sockets.emit('broadcast_user', {
             username: socket.username
         });
@@ -155,11 +152,14 @@ io.on("connection", (socket) => {
     });
 
     socket.on("update_name", (data) => {
-        writechat(socket.username + ":" + data.message)
-        io.sockets.emit("broadcast_updated_name", {
-            old_user: socket.username,
-            username: data.name,
-        })
+        if (data.name) {
+            writechat(socket.username + ": " + data.message)
+            io.sockets.emit("broadcast_updated_name", {
+                old_user: socket.username,
+                username: data.name,
+            })
+        
+
         for (var i = 0; i < userlist.length; i++) {
             if (userlist[i] == socket.username) {
                 userlist[i] = data.name;
@@ -167,9 +167,10 @@ io.on("connection", (socket) => {
         }
         socket.username = data.name;
         io.sockets.emit("broadcast_userlist", userlist)
+    }
     });
 
-    socket.on("disconnect", (data) => {
+    socket.on("disconnect", () => {
         writelog("User " + socket.username + " disconnected");
         for (var i = 0; i < userlist.length; i++) {
             if (userlist[i] == socket.username) {
--- versao helder/public/chat.js
+++ versao helder/public/chat.js
@@ -29,9 +29,15 @@ $(function () {
     //Listen on new_message
     socket.on("broadcast_message", (data) => {
         chatroom.append("<p class='message'>" + data.username + ": " + data.message + "</p>");
+        onclick = document.getElementById('message').value = ""
     });
+    socket.on("render_img_chat", (data) => {
+        console.log(data.message);
+        chatroom.append("<p><img src='"+data.message+"'></p>")
+    })
     socket.on("broadcast_updated_name", (data) => {
         chatroom.append("<p class='alert alert-warning' role='alert'>" + data.old_user + " is now " + data.username + " </p>");
+        onclick = document.getElementById('name').value = ""
     })
     socket.on("broadcast_user", (data) => {
         chatroom.append("<p class='alert alert-primary' role='alert'>" + data.username + " connected </p>");
@@ -39,16 +45,15 @@ $(function () {
     socket.on("broadcast_user_disconnect", (data) => {
         chatroom.append("<p class='alert alert-danger' role='alert'>" + data.username + " disconnected </p>");
     })
-    socket.on('private_message',function(data){
+    socket.on('private_message', function (data) {
         chatroom.append("<span class=privado><p>User " + data.username + " send you : " + data.message + "</p></span>")
     })
-    socket.on('message_true',function(data){
+    socket.on('message_true', function (data) {
         chatroom.append("<span class=sus><p>You send to " + data.username + ": " + data.message + "</p></span>")
     })
-    socket.on('erro_user',function(data){
-        console.log("herre")
-        chatroom.append("<span class=erro><p>"+ data.message + "</p></span>")
-})
+    socket.on('erro_user', function (data) {
+        chatroom.append("<span class=erro><p>" + data.message + "</p></span>")
+    })
     socket.on("broadcast_userlist", (data) => {
         list.empty();
         for (i = 0; i < data.length; i++) {
